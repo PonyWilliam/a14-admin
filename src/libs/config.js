f4675449bbf43b88dddf83e9011347bba24ecfa7
小伟王 <tjw@dadiqq.cn>
2021-04-14 17:11:09 +0800

    fix some

---
+++ src/libs/config.js
@@ -0,0 +1,13 @@
+// config.js
+const config = {
+    url:"http://192.168.1.103:8080/",
+    work:"work/",
+    category:"product/category/",
+    area:"product/Area/",
+    product:'product/',
+    login:"login",
+    token:"token",
+    dataurl:'http://datasql.dadiqq.cn/',
+    rfidIp:'http://192.168.97.97:5888',
+}
+export default config
